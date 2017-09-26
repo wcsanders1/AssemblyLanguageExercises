72b9570aebb476d2da6d715d51607e636b25e565
William Sanders <william.sanders076@gmail.com>
2017-09-26 07:16:48 -0400

    4.9.2 problem 1

--- Chapter4/4.9.2.asm
+++ Chapter4/4.9.2.asm
@@ -10,8 +10,12 @@ three DWORD 12345678h
 main PROC
 
 ; 1.
-	
-	
+	mov		ax, WORD PTR three	
+	mov		bx, WORD PTR three + 2
+	mov		WORD PTR three, bx
+	mov		WORD PTR three + 2, ax
+
+
 	INVOKE ExitProcess,0
 main ENDP
 END main
