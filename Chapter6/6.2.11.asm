ac313c39f8b2d59ce486af7a3e0a2e778fdc9179
William Sanders <william.sanders076@gmail.com>
2017-11-28 07:30:55 -0500

    6.2.11 questions 1-4

--- Chapter6/6.2.11.asm
+++ Chapter6/6.2.11.asm
@@ -11,6 +11,20 @@ INCLUDE Irvine32.inc
 .code
 main PROC
 
+; ---------------------------------- 1.
+	xor		ah, ah
+
+; ---------------------------------- 2.
+	or		ah, 0FFh
+
+; ---------------------------------- 3.
+	xor		eax, 0FFFFFFFFh
+
+; ---------------------------------- 4.
+	mov		eax, 4d		; move even number into eax to test
+	and		eax, 1b
+	mov		eax, 3d		; move odd number into eax to test
+	and		eax, 1b
 
 	INVOKE ExitProcess,0
 main ENDP
