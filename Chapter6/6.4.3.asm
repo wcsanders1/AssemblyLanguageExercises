5865cf14607f8df5acf48d852ef468f1779372fa
William Sanders <william.sanders076@gmail.com>
2017-12-05 07:28:36 -0500

    6.4.3 question 4

--- Chapter6/6.4.3.asm
+++ Chapter6/6.4.3.asm
@@ -6,11 +6,26 @@ ExitProcess PROTO, dwExitCode:DWORD
 INCLUDE Irvine32.inc
 
 .data
-
+array SWORD 3, 6, 1, 10, -10, -30, -40, -4
+sentinel SWORD 0
 
 .code
 main PROC
 
+	mov		esi, OFFSET array
+	mov		ecx, LENGTHOF array
+
+L1:
+	test	WORD PTR [esi], 8000h
+	pushfd
+	add		esi, TYPE array
+	popfd
+	loopz	L1
+
+	jnz		quit
+	sub		esi, TYPE array
+
+quit:
 
 	INVOKE ExitProcess,0
 main ENDP
