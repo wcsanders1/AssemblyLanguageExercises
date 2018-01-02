5b232e8b1d912239ddf1ca1709c03538b4516abc
William Sanders <william.sanders076@gmail.com>
2018-01-02 07:23:01 -0500

    6.10.2 questions 3, 4, 5, 6

--- Chapter6/6.10.2.asm
+++ Chapter6/6.10.2.asm
@@ -7,6 +7,8 @@ INCLUDE Irvine32.inc
 
 .data
 parityVar DWORD 1235d
+setX DWORD 00001111010110100000111100111100b
+setY DWORD 10100011010110100000111100011000b
 
 .code
 main PROC
@@ -19,6 +21,30 @@ main PROC
 	xor al, BYTE PTR parityVar + 1
 	xor al, BYTE PTR parityVar + 2
 	xor al, BYTE PTR parityVar + 3
+
+; ---------------------------------- 3.
+	mov eax, setX
+	xor eax, setY
+
+; ---------------------------------- 4.
+	cmp dx, cx
+	jbe L1
+
+L1:
+
+; ---------------------------------- 5.
+	cmp ax, cx
+	jg L2
+
+L2:
+
+; ---------------------------------- 6.
+	and al, 11111100b
+	jz L3
+	jmp L4 
+
+L3:
+L4:
 
 	INVOKE ExitProcess,0
 main ENDP
