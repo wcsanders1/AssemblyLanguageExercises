ad02517fbc32ea779b9d4867f8bfdadee3bfa4e0
William Sanders <william.sanders076@gmail.com>
2018-02-02 07:18:03 -0500

    7.1.10 question 5

--- Chapter7/7.1.10.asm
+++ Chapter7/7.1.10.asm
@@ -8,8 +8,18 @@ INCLUDE Irvine32.inc
 .code
 main PROC
 
+; ---------------------------------- 5.
 
+	mov eax, 1
+	mov ebx, 0
 
+	rcr ax, 1
+	rcr bx, 1
+
+	mov eax, 1
+	mov ebx, 0
+
+	shrd bx, ax, 1
 	INVOKE ExitProcess,0
 main ENDP
 END main
