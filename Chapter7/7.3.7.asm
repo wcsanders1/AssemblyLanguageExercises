81c7ec5a446a94e1cfc24c83c16dff1ab68e080c
William Sanders <william.sanders076@gmail.com>
2018-02-15 07:18:14 -0500

    7.3.7 question 7

--- Chapter7/7.3.7.asm
+++ Chapter7/7.3.7.asm
@@ -5,10 +5,18 @@ ExitProcess PROTO, dwExitCode:DWORD
 
 INCLUDE Irvine32.inc
 
+.data
+exVal SWORD -60
+
 .code
 main PROC
 
+; ---------------------------------- 7.
 
+	mov ax, exVal
+	cwd
+	mov bx, 30
+	idiv bx
 
 	INVOKE ExitProcess,0
 main ENDP
