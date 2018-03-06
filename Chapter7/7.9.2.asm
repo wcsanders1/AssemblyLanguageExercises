d089d71a8f06d5e6af66363a84fe692ddd07193d
William Sanders <william.sanders076@gmail.com>
2018-03-06 07:27:40 -0500

    7.9.2 questions 1, 2 (complete)

--- Chapter7/7.9.2.asm
+++ Chapter7/7.9.2.asm
@@ -12,23 +12,42 @@ main PROC
 
 ; ---------------------------------- 1.
 
-	mov ax, 0000000010000000b	; test value
+	mov ax, 1000000010000000b	; test value
 	xor ebx, ebx
 	mov bx, ax
 	shrd eax, ebx, 16d
 	jc Signed
-
+	
 	xor ebx, ebx
 	shrd eax, ebx, 16d
-
-	jmp ExitApp
-
+	
+	jmp Exit1
+	
 	Signed:
-
+	
 		mov ebx, 1111111111111111b
 		shrd eax, ebx, 16d
 
-	ExitApp:
+	; or you could just do this:
+	; shl eax, 16d
+	; sar eax, 16d
+	
+	Exit1:
+
+; ---------------------------------- 2.
+
+	mov al, 01010101b	; test value
+	shr al, 1
+	jnc Exit2
+
+	shl al, 1
+	mov ah, 00000001b
+	shr ax, 1
+
+	; or you could just do this:
+	; or al, 80h
+
+	Exit2:
 
 	call	Crlf
 	call	WaitMsg
