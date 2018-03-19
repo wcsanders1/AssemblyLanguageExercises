234d6915ff97060273cde6ece22cbff837675657
William Sanders <william.sanders076@gmail.com>
2018-03-19 07:27:20 -0400

    7.10 question 1 (incomplete)

--- Chapter7/7.10.asm
+++ Chapter7/7.10.asm
@@ -11,6 +11,15 @@ INCLUDE Irvine32.inc
 main PROC
 
 ; ---------------------------------- 1.
+	
+	.data
+	testNum1_1 BYTE '4321'
+
+	.code
+	mov ecx, LENGTHOF testNum1_1
+	mov edx, OFFSET testNum1_1
+	call WriteScaled
+	call Crlf
 
 ; ******** END OF QUESTIONS **********	
 
@@ -20,4 +29,44 @@ main PROC
 	INVOKE ExitProcess,0
 
 main ENDP
+
+WriteScaled PROC
+;-----------------------------------------------------------------------------
+; Outputs a decimal; ASCII number with an implied decimal point
+; Receives: EBX = decimal offset
+;			ECX = number length
+;			EDX = number's offset
+; Returns: nothing
+;-----------------------------------------------------------------------------
+
+	.data
+	num_offset DWORD 0
+	num_length DWORD 0
+	decimal_offset DWORD 0
+	cur_pos DWORD 0
+
+	.code
+	mov decimal_offset, ebx
+	mov num_length, ecx
+	mov num_offset, edx
+	pushad
+
+	mov edx, num_offset
+	add edx, num_length
+	mov cur_pos, edx
+	mov ebx, 0
+	mov ecx, num_length
+
+	printNum:
+
+		mov al, BYTE PTR [cur_pos + ebx]
+		call WriteChar
+		inc ebx
+		loop printNum
+
+	popad
+	ret
+
+WriteScaled ENDP
+
 END main
