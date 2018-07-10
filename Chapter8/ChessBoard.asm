52bc88502ca0f859097ff3c16dfd657da3598350
William Sanders <william.sanders076@gmail.com>
2018-07-10 07:02:38 -0400

    Begin proc to print line with alternating colors (incomplete)

--- Chapter8/ChessBoard.asm
+++ Chapter8/ChessBoard.asm
@@ -7,8 +7,12 @@ INCLUDE Irvine32.inc
 
 .code
 
+;*****************************************************************************
+;*****************************************************************************
+;*****************************************************************************
+
 SetColorXY PROC USES eax edx,
-	color: byte, xPos:byte, yPos:byte
+	color: byte, xPos: byte, yPos: byte
 ;-----------------------------------------------------------------------------
 ; Colors a space on the console
 ; Receives: X and Y coordinates and color
@@ -38,9 +42,61 @@ SetColorXY PROC USES eax edx,
 
 SetColorXY ENDP
 
+;*****************************************************************************
+;*****************************************************************************
+;*****************************************************************************
+
+PrintLineAlternatingColors PROC USES eax ecx edx,
+	lineLength: dword, yPos: byte, color1: byte, color2: byte
+;-----------------------------------------------------------------------------
+; Makes a line on the console with alternating colors
+; Receives: line length, y position, two colors
+; Returns: Nothing
+;-----------------------------------------------------------------------------
+	
+	local currentColor: byte
+
+	mov dh, yPos
+	mov dl, 0
+	mov ecx, lineLength
+	xor eax, eax
+	movzx eax, color1
+	mov currentColor, al
+
+	colorSpace:
+		
+		call Gotoxy
+		shl eax, 4
+		call SetTextColor
+		mov al, ' '
+		call WriteChar
+		inc dl
+
+		mov currentColor, al
+		cmp al, color1
+		je changeToColor2
+		
+		movzx eax, color1
+		mov currentColor, al
+		loop colorSpace
+
+		changeToColor2:
+
+			movzx eax, color2
+			mov currentColor, al
+			loop colorSpace
+	
+	ret
+
+PrintLineAlternatingColors ENDP
+
+;****************************************************************************
+;****************************************************************************
+;****************************************************************************
+
 main PROC
 
-	invoke SetColorXY, red, 20, 10
+	invoke PrintLineAlternatingColors, 10, 10, red, green
 
 	call	Crlf
 	call	WaitMsg
